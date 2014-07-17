8ff219d3456e3279b6fc3f77b96a14d4976a6145
don-E Merson <dmerson@email.arizona.edu>
2014-07-17 03:51:39 -0700

    Add a birthdate to the RegisterViewModel.

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -64,6 +64,8 @@ namespace UpdatingUserObjectDemo.Models
         [Display(Name = "Password")]
         public string Password { get; set; }
 
+        public System.DateTime DateOfBirth { get; set; }
+
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
