1ec47387ca5452c703dc15811517d7e2aa59dce4
don-E Merson <dmerson@email.arizona.edu>
2014-07-17 03:39:28 -0700

    After typing command enable-migrations a new folder is created with the initial create class.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace UpdatingUserObjectDemo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UpdatingUserObjectDemo.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "UpdatingUserObjectDemo.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(UpdatingUserObjectDemo.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
