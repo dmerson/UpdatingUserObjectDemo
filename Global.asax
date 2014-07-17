7d46adea85d3350286dbc3aea38472ca8d09f91c
don-E Merson <dmerson@email.arizona.edu>
2014-07-17 03:31:37 -0700

    Start of application

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UpdatingUserObjectDemo.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UpdatingUserObjectDemo.Startup))]
+namespace UpdatingUserObjectDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
